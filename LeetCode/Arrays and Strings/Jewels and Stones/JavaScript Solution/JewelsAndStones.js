a6a29397321ad24a031b66f99fe40e1370ac75e2
kamal11 <kyzx11@gmail.com>
2018-09-17 15:05:36 +0530

    Javascript solution of LeetCode JewelsAndStones

--- LeetCode/Arrays and Strings/Jewels and Stones/JavaScript Solution/JewelsAndStones.js
+++ LeetCode/Arrays and Strings/Jewels and Stones/JavaScript Solution/JewelsAndStones.js
@@ -5,16 +5,6 @@
  */
 var numJewelsInStones = function(J, S) {
     const hash = {}; let count = 0;
-    // for(let char of S) {
-    //     hash[char] = hash[char] + 1 || 1;
-    // }
-    
-    // for(let char in hash) {
-    //     console.log(J[hash[char]])
-    //    // if(char == J[hash[char]]) {
-    //    //      count++;
-    //    //  }
-    // }
     for(let i = 0; i < S.length; i++) {
         for(let j = 0; j < J.length; j++) {
             if(S[i] === J[j]) {
